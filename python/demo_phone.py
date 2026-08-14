8df3f79d917537d4b86ca969184683afa4babb07
Lychqq <lychqq@example.com>
2026-08-14 10:37:41 +0300

    test: re-enable threads for full android test

--- python/demo_phone.py
+++ python/demo_phone.py
@@ -3,12 +3,14 @@ from tachyon_edge import Tachyon
 
 print("Starting Tachyon Node on Phone...")
 node = Tachyon(17001, "test_secret")
-print("INIT OK! No panic so far.")
+print("INIT OK!")
 
-time.sleep(1)
+node.start()
+print("START OK!")
 
-# node.start()
-# print("START OK")
+node.start_discovery()
+print("DISCOVERY OK! Searching for PC over Wi-Fi...")
 
-# node.start_discovery()
-# print("DISCOVERY OK")
+time.sleep(30)
+print("Done.")
+node.stop()
