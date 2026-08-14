e0ca0d7268dd5d47f4bcc27288469f66165d4141
Lychqq <lychqq@example.com>
2026-08-14 10:36:18 +0300

    fix: combine getauxval hack and std_options fix

--- src/capi.zig
+++ src/capi.zig
@@ -8,6 +8,17 @@ pub const std_options: std.Options = .{
 };
 const builtin = @import("builtin");
 
+comptime {
+    if (builtin.os.tag == .linux and builtin.abi == .android) {
+        @export(&dummy_getauxval, .{ .name = "getauxval", .linkage = .strong });
+    }
+}
+
+pub fn dummy_getauxval(type_: c_ulong) callconv(.c) c_ulong {
+    _ = type_;
+    return 0; // Hardware capabilities fallback
+}
+
 // Safe C-FFI representation of a packet
 pub const CPacket = extern struct {
     topic_ptr: [*]const u8,
